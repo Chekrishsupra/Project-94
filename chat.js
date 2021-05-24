938f0c9866de3eae1c6cfa2188592520f24e85bd
Chekrishsuper <78082844+Chekrishsupra@users.noreply.github.com>
2021-05-24 09:52:28 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAk2iZ8mvSP0z3pj0qy9TywI-Tk5z0CXMU",
+    authDomain: "letschat-282b7.firebaseapp.com",
+    projectId: "letschat-282b7",
+    storageBucket: "letschat-282b7.appspot.com",
+    messagingSenderId: "611084427500",
+    appId: "1:611084427500:web:d52a85e4e822015b5f85c9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
